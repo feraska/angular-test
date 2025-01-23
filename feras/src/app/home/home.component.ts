8414b70b2d86990c219708ce61d0c32f988cf149
feraska <feras.94@hotmail.com>
2025-01-23 14:00:38 +0200

    dynamic style

--- feras/src/app/home/home.component.ts
+++ feras/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { afterRender, AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnInit, Renderer2 } from '@angular/core';
 import { ReactiveFormsModule,FormGroup,FormControl } from '@angular/forms';
 import {ApiService} from "../api.service"
 import { Info } from '../info';
@@ -10,20 +10,37 @@ import { Observable } from 'rxjs';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit{
+
+export class HomeComponent implements OnInit,AfterViewInit{
   applyForm = new FormGroup({
   firstName:new FormControl('')
   
   })
   posts: Info[] = []
+   
   ngOnInit(): void {
       this.api.getPosts("https://jsonplaceholder.typicode.com/posts").subscribe((data)=>{
         this.posts = data
+        
       })
+      
+      
+     
   }
   constructor(private api:ApiService){
-
+    afterRender(()=> {
+      // elementRef.nativeElement.querySelector('input')?.focus()
+      const input = document.getElementsByTagName('input')[0] as HTMLElement
+      input?.focus()
+      const element = document.getElementsByClassName('feras')[0] as HTMLElement
+      element?.style?.setProperty('display','none')
+    })
   }
+  ngAfterViewInit(): void {
+    
+      
+      
+    }
   submit() {
    //this.api.submitApplication(this.applyForm.value.firstName??'feras')
    console.log(this.applyForm.controls.firstName.value)
